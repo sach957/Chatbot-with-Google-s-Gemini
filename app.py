52cd082099ad9eab5f079d3a3167c089c7c0e589
Sachin Nagal <145531993+sach957@users.noreply.github.com>
2024-04-24 16:10:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,28 @@
 import google.generativeai as genai
+
 import warnings 
 warnings.filterwarnings('ignore')
-
-from IPython.display import Markdown
+from dotenv import load_dotenv
+load_dotenv()
 
 import streamlit as st
 key="AIzaSyDgkL5jUfoLvXk5v8Xe7USiYGpn0NwT_0w"
-genai.configure(api_key=key)
 
-def get_genai_response(question="Welcome"):
-    model=genai.GenerativeModel("gemini-pro")
-    response=model.generate_content(question)
-    return response.text
+import os
 
-# sam = "write a pick up to ask for a date"
-# response = get_genai_response(sam)
-# print(response)
+from langchain_google_genai import ChatGoogleGenerativeAI
+model = ChatGoogleGenerativeAI(model="gemini-pro")
+
+def get_genai_response(question):
+    response=model.invoke(question)
+    return response.content
+
 
 st.set_page_config(page_title="Q&A Demo")
 
 st.header("Gemini Pro App")
 
-ques=st.text_input("Input: ",key="input")
+ques=st.text_input("Input the question you want to ask: ",key="input")
 
 submit = st.button("Ask the Quesion")
 
@@ -29,3 +30,26 @@ if submit:
     response = get_genai_response(ques)
     st.subheader("The Response is")
     st.write(response)
+
+
+
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+def prom(place):
+    prompt_template = PromptTemplate(input_variables = ['country'],
+                                     template="Tell me the capital of {country}")
+
+    chain = LLMChain(llm=model,prompt=prompt_template)
+    response=chain.invoke(place)
+    return response['text']
+
+
+ques2=st.text_input("Input the country/state whose capital you want to know: ",key="input2")
+
+submit2 = st.button("Enter the state/country")
+
+if submit2:
+    response2 = prom(ques2)
+    st.subheader("The Response is")
+    st.write(response2)
